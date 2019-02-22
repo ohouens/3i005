00ca4c5ca28e23290e8ee8e0eb1815b206483ebc
ohouens <rohouens@yahoo.fr>
2019-02-22 17:22:39 +0100

    ajout monte carlo

--- projet_1/monte-carlo.py
+++ projet_1/monte-carlo.py
@@ -1,36 +1,56 @@
 from source import *
 import random
 import math
+import copy
 
 class AgentAlea(Agent):
 	def __init__(self, prenom="zero"):
 		self.prenom = prenom
-		print("un joueur initialisé: "+self.prenom)
+		#print("un joueur initialisé: "+self.prenom)
 
 	def get_action(self,state):
 		case = random.randint(0, len(state.get_actions())-1)
 		return state.get_actions()[case]
 
 
-# state = MorpionState()
-# print(state.get_actions())
-# print(state.grid)
-# state.grid[0][0] = -1
-# print(state.grid)
-# print(state.get_actions())
+class AgentMontecarlo(Agent):
+	def __init__(self, prenom="zero", state=MorpionState()):
+			self.prenom = prenom
+			self.state = state
+			#print("un joueur initialisé: "+self.prenom)
 
+	def get_action(self,state,n=50):
+		moyennes = []
+		coups = []
+		esperance = []
+		actions = state.get_actions()
+		j1 = AgentAlea("j1")
+		j2 = AgentAlea("j2")
 
-"""
-jeu = Jeu(MorpionState(), AgentAlea(), AgentAlea())
-win, log = jeu.run(True, 1)
-print("joueur gagnant: "+str(win))
-print(log)
-"""
+		for j in range (len(actions)):
+			moyennes.append(0)
+			esperance.append(0)
+			coups.append(0)
 
-def jeuxAleatoire(T=1000):
-	j1 = AgentAlea("Ryan")
-	j2 = AgentAlea("Pierre")
+		for i in range(n):
+			#print('partie '+str(i)+' aleatoire')
+			choix = random.randint(0,len(actions)-1)
+			coups[choix] += 1
+			#print("actions: "+str(len(actions)))
+			#print('choix: '+str(choix))
+			newState = copy.deepcopy(state)
+			v1, v2 = jeux(newState, j1, j2, 60, False)
+			moyennes[choix] += v1
 
+		for i in range(len(coups)):
+			if[coups[i]==0]:
+				esperance[i] = 0
+			else:
+				esperance[i] = moyennes[i]/coups[i]
+
+		return (actions[esperance.index(max(esperance))])
+
+def jeux(state, j1, j2, T=500, show=True):
 	somme1 = 0
 	somme2 = 0
 
@@ -39,7 +59,7 @@ def jeuxAleatoire(T=1000):
 	partiesJ2 = []
 
 	for i in range(T):
-		jeu = Jeu(MorpionState(), j1, j2)
+		jeu = Jeu(state, j1, j2)
 		win, log = jeu.run()
 		if(win == 1):
 			somme1 += 1
@@ -49,54 +69,15 @@ def jeuxAleatoire(T=1000):
 		partiesJ1.append(somme1)
 		partiesJ2.append(somme2)
 
-	print('Poucentage:\nJ1: '+str(somme1*100/T)+'\nJ2: '+str(somme2*100/T))
-	plt.plot(x, partiesJ1, label='j1')
-	plt.plot(x, partiesJ2, label='j2')
-	plt.xlabel('Times(moves)')
-	plt.ylabel('parties gagnees')
-	plt.title('Morpions')
-	plt.legend()
-	plt.show()
+	if(show):
+		print('Pourcentage:\n'+j1.prenom+': '+str(somme1*100/T)+'\n'+j2.prenom+': '+str(somme2*100/T))
+		plt.plot(x, partiesJ1, label='j1')
+		plt.plot(x, partiesJ2, label='j2')
+		plt.xlabel('Times(moves)')
+		plt.ylabel('parties gagnees')
+		plt.title('Morpions')
+		plt.legend()
+		plt.show()
+	return (somme1*100/T, somme2*100/T)
 
-# jeuxAleatoire()
-
-class AgentMontecarlo(Agent):
-	def __init__(self, prenom="zero"):
-			self.prenom = prenom
-			print("un joueur initialisé: "+self.prenom)
-
-	def get_action(self,state,n=3):
-		moyennes = []
-		actions = state.get_actions()
-		j1 = AgentAlea("j1")
-		j2 = AgentAlea("j2")
-
-		
-		for j in range (len(actions)):
-			moyennes.append(0)
-
-		for i in range(n+1):
-			choix = random.randint(0,len(actions))
-			print(choix)
-			a = state
-			while(a.win()==0):
-				jeu = Jeu(a, j1, j2)
-				gagnant, log = jeu.run()
-				"faire jouer les joueurs aléatoirement"
-				if(gagnant == 1):
-					moyennes[choix] += 1
-
-			
-		return (actions[index(max(moyennes))])
-		"mettre à jour moyennes en fonction du resultat"
-		"retourner max(moyennes)"
-
-		
-
-
-j1 = AgentMontecarlo("Pierre")
-j2 = AgentAlea("Ryan")
-jeu = Jeu(MorpionState(), j1, j2)
-win, log = jeu.run()
-print("joueur gagnant: "+str(win))
-print(log)
+jeux(MorpionState(), AgentAlea("Pierre"), AgentMontecarlo("Ryan"), 40)
